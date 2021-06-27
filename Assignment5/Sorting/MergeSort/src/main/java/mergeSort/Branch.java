ed4af12200267d0d42151cdfd13a9da0cd6e938e
Ivan Fernandez <iafernan@asu.edu>
2021-06-27 02:21:11 -0700

    Start 2

--- Assignment5/Sorting/MergeSort/src/main/java/mergeSort/Branch.java
+++ Assignment5/Sorting/MergeSort/src/main/java/mergeSort/Branch.java
@@ -98,7 +98,7 @@ public class Branch extends Node {
   }
 
   public static void main(String[] args) {
-    new Branch(Integer.valueOf(args[0]), Integer.valueOf(args[1]), Integer.valueOf(args[2]), "localhost", "localhost").run();
+    new Branch(Integer.valueOf(args[0]), Integer.valueOf(args[1]), Integer.valueOf(args[2]), "3.20.233.20", "3.20.233.20").run();
   }
 
 
